1ccb9f48d6f97d3800d62d3a9b20fd1f512fab36
Kübra <47282740+kbratprksvn@users.noreply.github.com>
2023-12-14 00:21:29 +0300

    web sitesi
    web sitesi

---
+++ Projem WEB/index.php
@@ -0,0 +1,175 @@
+  <!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/17043a8027.js" crossorigin="anonymous"></script>
+    <title>ROBOTİK KODLAMA ATÖLYESİ ANASAYFA</title>
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;1,400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="owl/owl.carousel.min.css">
+<link rel="stylesheet" href="owl/owl.theme.default.min.css">
+</head>
+<body>
+    <section id="menu">
+        <div id="logo"> ROBOTİK KODLAMA ATÖLYESİ</div>
+        <nav> 
+            <a href=""> <i class="fa-solid fa-house ikon" ></i>Anasayfa</a>
+            <a href=""><i class="fa-solid fa-circle-info ikon"></i>Hakkımızda</a>
+            <a href=""><i class="fa-solid fa-camera-retro ikon"></i>Foto Galeri</a>
+            <a href=""><i class="fa-solid fa-user-astronaut ikon"></i>Eğitim İçeriği</a>
+            <a href=""><i class="fa-solid fa-users-line ikon"></i>Ekip</a>
+            <a href=""><i class="fa-solid fa-phone ikon"></i>İletişim</a>
+        </nav>
+    </section>
+    <section id="banner">
+        <div id="black"> 
+        </div>
+        <div id="icerik">
+          <h2>Robotik Kodlama Atölyesi</h2>
+          <hr width=300 align:left>
+           <p> Robotik Kodlama Atölyesi, robotik ve kodlama dünyasına giriş yapmayı sağlayan çok yönlü bir atölyedir. Bu atölyenin birincil amacı kodlamanın temel kurallarını öğretmek ve teknoloji ile yaratıcılığı birleştirerek sunmaktır.
+        </p>
+        </div>
+    </section>
+    <section id="hakkimizda">
+        <h3>Hakkımızda</h3>
+        <div class="container">
+        <div id="sol">
+            <h5 id="h5sol">
+                Robotik Kodlama Atölyesi Hakkında !
+            </h5>
+        </div>
+        <div id="sag">
+             <span>R</span>
+             <p id="psag">obotik kodlamanın önemi, lego teknik set ile öğrencilerin günlük hayat da kullandığımız teknolojik aletlerin küçük maketlerini yaparak çalışma mantığını anlamaktadır.
+
+             </p>
+        </div>
+        <img src="Resimler/banneralti.webp" alt="" class="img-fluid mt100">
+        <p id="pson"> Etkin ve yaygın çözüm ortaklığı ve network yapısını bir avantaj olarak toplumun tüm kesimlerine yaygınlaştırmak üzere yapılanmasını tamamlamıştır</p>
+        </div>
+    </section>
+    <section id="galeri">
+        <div class="container">
+            <h3>Foto Galeri</h3>
+            <div class="owl-carousel owl-theme">
+                <div class="card item" data-merge=1.5 >
+                    <img src="Resimler/banner.jpg" alt="" class="img-fluid">
+                </div>
+                <div class="card item" data-merge=1.5>
+                    <img src="Resimler/banneriki.jpg" alt="" class="img-fluid">
+                </div>
+                <div class="card item" data-merge=1.5>
+                    <img src="Resimler/banneruc.jpg" alt="" class="img-fluid">
+                </div>
+                <div class="card item" data-merge=1.5>
+                    <img src="Resimler/bannerdort.jpg" alt="" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="egitim">
+        <div class="container">
+            <h3>Eğitim İçeriği</h3>
+        <div class="card" data-merge=1.5><h1 id="h1">
+            Robotik Kodlama Atölyesi Hakkında !
+        </h1>                    <img src="Resimler/banner.jpg" alt="" class="img-fluid">
+    </div>
+        <div class="card" data-merge=1.5><h1 id="h1iki">
+            Robotik Kodlama Atölyesi Hakkında !
+        </h1>                    <img src="Resimler/banner.jpg" alt="" class="img-fluid">
+    </div>
+        <div class="card" data-merge=1.5><h1 id="h1uc">
+            Robotik Kodlama Atölyesi Hakkında !
+        </h1>                    <img src="Resimler/banner.jpg" alt="" class="img-fluid">
+    </div>
+        </div>
+    </section>
+    <section id="ekip">
+       <div class="container">
+        <h3> EKİP</h3>
+        <div class="sutun-sol-sag">
+            <img src="Resimler/bannerfoti.jpg" alt=""  class="img-fluid sekil">
+            <h4 class="ekipisim">Kübra Toprakseven</h4>
+            <p class="ekipp">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            </p>
+            <div class="ekip-ikon">
+            <a href="#"> <i class="fa-brands fa-square-whatsapp social"></i></a>
+            <a href=""><i class="fa-brands fa-square-instagram social"></i></a>
+            <a href=""><i class="fa-brands fa-square-twitter social"></i></a>
+            </div>
+        </div>
+        <div class="sutun-sol-sag">
+            <img src="Resimler/banneralti.webp" alt=""  class="img-fluid sekil">
+            <h4 class="ekipisim">Kübra Toprakseven</h4>
+            <p class="ekipp">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            </p>
+            <div class="ekip-ikon">
+            <a href="#"> <i class="fa-brands fa-square-whatsapp social"></i></a>
+            <a href=""><i class="fa-brands fa-square-instagram social"></i></a>
+            <a href=""><i class="fa-brands fa-square-twitter social"></i></a>
+            </div>
+        </div>
+        <div class="sutun-sol-sag">
+            <img src="Resimler/bannerdort.jpg" alt=""  class="img-fluid sekil">
+            <h4 class="ekipisim">Kübra Toprakseven</h4>
+            <p class="ekipp">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            </p>
+            <div class="ekip-ikon">
+            <a href="#"> <i class="fa-brands fa-square-whatsapp social"></i></a>
+            <a href=""><i class="fa-brands fa-square-instagram social"></i></a>
+            <a href=""><i class="fa-brands fa-square-twitter social"></i></a>
+            </div>
+        </div>
+       </div> 
+    </section>
+    <section id="iletisim">
+        <div class="container">
+            <h3 id="h3iletisim">İletişim</h3>
+<div id="iletisimopak">
+    <div id="formgroup">
+        <div id="solform">
+            <input type="text" name="isim" placeholder="Ad Soyad" required class="form-control">
+            <input type="text" name="tel"placeholder="Telefon Numarası" required class="form-control">
+        </div>
+        <div id="sagform">
+            <input type="text" name="mail" placeholder="Email Adresi" required class="form-control">
+            <input type="text" name="konu"placeholder="Konu Başlığı" required class="form-control">
+        </div>      
+        <textarea name="mesaj" id="" cols="30" placeholder="Mesaj Giriniz" rows="10" required class="form-control"></textarea>
+        <input type="submit" value="Gönder">
+    </div>
+        <div id="adres">
+<h4 id="adresbaslik">Adres:</h4>
+<p class="adresp">............... mah</p>
+<p class="adresp">...................... cad.</p>
+<p class="adresp">............... no:0</p>
+<p class="adresp">0000 000 00 00</p>
+<p class="adresp">Email:......@gmail.com</p>
+  </div>
+</div>
+<footer>
+<div id="copyright">Robotik Kodlama Atölyesi</div>
+<div>
+    <a href="#"> <i class="fa-brands fa-square-whatsapp social" id="ikoni"></i></a>
+    <a href=""><i class="fa-brands fa-square-instagram social" id="ikoni"></i></a>
+    <a href=""><i class="fa-brands fa-square-twitter social" id="ikoni"></i></a>
+</div>
+<a href="#menu"><i class="fas fa-angle-up" id="up"></i></a>
+</footer>
+        </div>
+
+    </section>
+    <script
+
+  src="https://code.jquery.com/jquery-3.7.1.slim.min.js"
+  integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8="
+  crossorigin="anonymous"></script>
+    <script src="owl/owl.carousel.min.js">
+</script>
+  <script src="owl/scriptt.js"></script>
+</body>
+</html>
---
+++ Projem WEB/panel.php
@@ -0,0 +1,54 @@
+
+<head>
+    <style>
+    #customers {
+      font-family: Arial, Helvetica, sans-serif;
+      border-collapse: collapse;
+      width: 100%;
+    }
+    
+    #customers td, #customers th {
+      border: 1px solid #ddd;
+      padding: 8px;
+    }
+    
+    #customers tr:nth-child(even){background-color: #f2f2f2;}
+    
+    #customers tr:hover {background-color: #ddd;}
+    
+    #customers th {
+      padding-top: 12px;
+      padding-bottom: 12px;
+      text-align: left;
+      background-color: #04AA6D;
+      color: white;
+    }
+    </style>
+    </head>
+    <body>
+    
+    <h1>PANEL</h1>
+    
+    <table id="customers">
+      <tr>
+        <th>Ad Soyad</th>
+        <th>Telefon</th>
+        <th>Mail</th>
+        <th>Konu Başlığı</th>
+        <th>Mesaj</th>
+      </tr>
+      <tr>
+        <td> Kübra T</td>
+        <td>05555555555</td>
+        <td>bsdjkhjaskd@gmail.com</td>
+        <td>deneme</td>
+        <td>ajadhasjdhjkasdhadjahjskdjkahdjahdajdkajdhk</td>
+      </tr>
+
+    </table>
+    
+    </body>
+    </html>
+    
+    
+    
